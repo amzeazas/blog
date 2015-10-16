96eefdc6d1cdaa5e3ff773f95628a058d07a08f0
Alexa <amzeazas@comcast.net>
2015-10-16 12:04:13 -0700

    Add new post functionality.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,31 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @tags = Tag.all
     render :index
   end
 
   def show
     @post = Post.find(params[:id])
     render :show
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :tag_ids)
   end
 end
