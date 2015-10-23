904a43db74ded56a2aaf103f293ef0112f43a5a8
Alexa <amzeazas@comcast.net>
2015-10-22 20:16:49 -0700

    Add create new comment functionality.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,23 @@
 class CommentsController < ApplicationController
-  
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+    render :new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment successfully created"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
