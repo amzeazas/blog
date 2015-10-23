7f8c058b656323c3c9b40ac7959c2852ab219b6c
Alexa <amzeazas@comcast.net>
2015-10-22 20:24:00 -0700

    Add update comment functionality.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @post= Post.find(@comment.post.id)
+    render :edit
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @post= Post.find(@comment.post.id)
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text)
